a7c03d28ad81e3a1f9efd6f0d84417cfc0931f2c
Gbadamosi Tolulope <tolux17@gmail.com>
2022-07-28 01:34:48 -0500

    added extra

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -24,7 +24,7 @@ class Docker implements Serializable {
     }
 
     def deployApp(){
-        echo "Application is deploying like yesterday"
+        script.echo "Application is deploying like yesterday"
     }
 
 
