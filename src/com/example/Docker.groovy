d0fb81ad5e10a84ebfce72de71980ac8f9493228
Gbadamosi Tolulope <tolux17@gmail.com>
2022-07-27 19:40:18 -0500

    used groovy class

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,7 +12,7 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def buildDockerImage (String) {
+    def buildDockerImage (String IMAGENAME) {
         script.withCredentials([script.usernamePassword(credentialsId:"Dockerhub", usernameVariable:"user",passwordVariable:"pass")]) {
         script.sh "docker build . -t $IMAGENAME"
         script.sh "echo $script.pass | docker login -u $script.user --password-stdin"
